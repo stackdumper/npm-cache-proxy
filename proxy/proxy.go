1ad9c743ec51c7d3308992c0f540583255f26332
 Ilya Atamas <stackdumper@gmail.com>
2019-04-22 19:38:56 +0300

    docs: fix missing directory in godoc

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -2,7 +2,7 @@
 // Package proxy implements a HTTP caching proxy for Node package registry (NPM).
 // See https://github.com/emeralt/npm-cache-proxy/ for more information about proxy.
 //
-package proxy // import "https://github.com/emeralt/npm-cache-proxy/proxy"
+package proxy
 
 import (
 	"net/http"
